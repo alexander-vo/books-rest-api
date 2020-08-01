fdf5272b1b0a373813d0c4635d9d65f4d5351f97
Alexander <voloshchenkoalexander@gmail.com>
2020-08-01 16:44:31 +0300

    configs done for now all config classes should be described in README later

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
+import os
 from books import create_app
-from config import DevelopmentConfig
+from config import *
 
 
 if __name__ == '__main__':
-    app = create_app(DevelopmentConfig)
+    env = os.getenv('ENV')
+
+    # Choose the environment config
+    if env == 'prod':
+        config = Config
+    elif env == 'test':
+        config = TestingConfig
+    else:
+        config = DevelopmentConfig
+
+    # Creating the app with chosen configuration
+    app = create_app(config)
     app.run()
--- books/__init__.py
+++ books/__init__.py
@@ -4,6 +4,11 @@ from config import Config
 
 
 def create_app(app_config: Type[Config] = Config) -> Flask:
+    """
+    Creating a flask application with configs
+    :param app_config: config class
+    :return: Flask object
+    """
     app = Flask(__name__)
-    app.config.from_object(app_config)
+    app.config.from_object(app_config())
     return app
